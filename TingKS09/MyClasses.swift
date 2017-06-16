6d38e833f8476befcf4682e62cbb93d7ad5492b4
user <user@userde-MacBook-2.local>
2017-06-16 09:32:34 +0800

    20170616

---
+++ TingKS09/MyClasses.swift
@@ -0,0 +1,26 @@
+//
+//  MyClasses.swift
+//  TingKS09
+//
+//  Created by user on 2017/6/16.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+import Foundation
+
+class MyClass01{}
+
+class MyClass02{
+    var x:Int
+    init(x:Int){self.x = x}
+}
+class MyClass03{
+    var x = 123
+    init(){}
+}
+class MyClass04{
+    var x:Int?
+}
+class MyClass05{
+    
+}
